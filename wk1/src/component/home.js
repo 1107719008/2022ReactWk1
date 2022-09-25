a50636f41b24893f1b89ace922797603c16390f4
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-09-25 15:02:01 +0800

    init proj

---
+++ wk1/src/component/home.js
@@ -0,0 +1,382 @@
+export default function home(){
+    return(
+
+        <div className="blockone">
+            <div className="navbg">
+                <p className="title">T&E</p>
+        
+        
+                <ul className="nav">
+                    <li className="navitem">ABOUT</li>
+                    <li className="navitem">NEW</li>
+                    <li className="navitem">ARTIS</li>
+                    <li className="navitem">ITEM</li>
+                    <li className="navitem">SAL</li>
+                </ul>
+                <div className="icon">
+                    <div className="ham_s">
+                    <img className="ics1" src="img/S-320/漢堡 icon.png" alt=""/>
+                    <img className="ics2" src="img/S-320/購物車 icon.png" alt=""/>
+                    <img className="ics3" src="img/S-320/使用者 icon.png" alt=""/>
+                    <img className="ics4" src="img/S-320/搜尋 icon.png" alt=""/>
+                    </div>
+                    <img className="gooo" src="img/L-1920/購物車.png"/>
+                    <img className="usericon" src="img/L-1920/使用者.png"/>
+                    <img className="searchicon" src="img/L-1920/搜尋.png"/>
+                </div>
+            </div>
+            <div className="partone">
+                <img className="title_s"src="img/S-320/Title.png" alt=""/>
+                <img className="bigimg_s"src="img/S-320/big IMG.jpg" alt=""/>
+        
+                <img className="m_title" src="img/M-768/Title.png" />
+                <img className="m_bigimg" src="img/M-768/big IMG.jpg" />
+                <img className="titlepic" src="img/L-1920/Title.png" / >
+
+                <p className="bigpic"><img className="bigimgpic" src="img/L-1920/big IMG.png"/></p>
+                <img className="today" src="img/L-1920/TODAY AND EVERYDAY.png"/>
+            
+            </div>
+        
+        <div className="itemtable">
+            <div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/L-1920/1.jpg" />
+                    <img className="itemrowone" src="img/L-1920/2.jpg" />
+                    <img className="itemrowone" src="img/L-1920/3.jpg" />
+                    <img className="itemrowone" src="img/L-1920/4.jpg" />
+                    <img className="itemrowone" src="img/L-1920/5.jpg" />
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+        
+                <div className="rowone">
+                    <img className="itemrowone" src="img/L-1920/6.jpg" />
+                    <img className="itemrowone" src="img/L-1920/7.jpg" />
+                    <img className="itemrowone" src="img/L-1920/8.jpg" />
+                    <img className="itemrowone" src="img/L-1920/9.jpg" />
+                    <img className="itemrowone" src="img/L-1920/10.jpg" />
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/L-1920/11.jpg" />
+                    <img className="itemrowone" src="img/L-1920/12.jpg" />
+                    <img className="itemrowone" src="img/L-1920/13.jpg" />
+                    <img className="itemrowone" src="img/L-1920/14.jpg" />
+                    <img className="itemrowone" src="img/L-1920/15.jpg" />
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/L-1920/15.jpg" />
+                    <img className="itemrowone" src="img/L-1920/16.jpg" />
+                    <img className="itemrowone" src="img/L-1920/17.jpg" />
+                    <img className="itemrowone" src="img/L-1920/18.jpg" />
+                    <img className="itemrowone" src="img/L-1920/19.jpg" />
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+        
+                
+            </div>
+        </div>
+        
+        
+        <div className="itemtable_m">
+            <div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/M-768/1.jpg" />
+                    <img className="itemrowone" src="img/M-768/2.jpg" />
+                    <img className="itemrowone" src="img/M-768/3.jpg" />
+                    <img className="itemrowone" src="img/M-768/4.jpg" />
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+        
+                <div className="rowone">
+                    <img className="itemrowone" src="img/M-768/6.jpg" />
+                    <img className="itemrowone" src="img/M-768/7.jpg" />
+                    <img className="itemrowone" src="img/M-768/8.jpg" />
+                    <img className="itemrowone" src="img/M-768/9.jpg" />
+                    
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                   
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/M-768/11.jpg" />
+                    <img className="itemrowone" src="img/M-768/12.jpg" />
+                    <img className="itemrowone" src="img/M-768/13.jpg" />
+                    <img className="itemrowone" src="img/M-768/14.jpg" />
+                    
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                   
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/M-768/15.jpg" />
+                    <img className="itemrowone" src="img/M-768/16.jpg" />
+                    <img className="itemrowone" src="img/M-768/17.jpg" />
+                    <img className="itemrowone" src="img/M-768/18.jpg" />
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+        
+                
+            </div>
+        </div>
+        <div className="itemtable_s">
+            <div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/1.jpg" />
+                    <img className="itemrowone" src="img/S-320/2.jpg" />
+                
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                   
+                
+                </div>
+        
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/6.jpg" />
+                    <img className="itemrowone" src="img/S-320/7.jpg" />
+                 
+                    
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+               
+                   
+                </div>
+                <div className="rowone">
+                  
+                    <img className="itemrowone" src="img/S-320/13.jpg" />
+                    <img className="itemrowone" src="img/S-320/14.jpg" />
+                    
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                
+                   
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/10.jpg" />
+                    <img className="itemrowone" src="img/S-320/16.jpg" />
+            
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                 
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/8.jpg" />
+                    <img className="itemrowone" src="img/S-320/9.jpg" />
+                    
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                   
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/3.jpg" />
+                    <img className="itemrowone" src="img/S-320/4.jpg" />
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+               
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/15.jpg" />
+                    <img className="itemrowone" src="img/S-320/16.jpg" />
+                   
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+                <div className="rowone">
+                    <img className="itemrowone" src="img/S-320/17.jpg" />
+                    <img className="itemrowone" src="img/S-320/18.jpg" />
+                   
+                </div>
+                <div className="rowtextone">
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                    <p className="textsty">
+                        安藤由香<br/>一輪挿し<br/>$1,234
+                    </p>
+                </div>
+        
+                
+            </div>
+            </div>
+        
+        
+        <div>
+           <p className="number">
+            12345
+           </p>
+        </div>
+        
+        
+        
+    </div>
+
+
+
+
+
+
+    );
+
+}
