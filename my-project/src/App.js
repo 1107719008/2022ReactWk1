70f4957c3fa4e32672767fe64af7263ee3224185
1107719008 <48972788+1107719008@users.noreply.github.com>
2022-09-26 01:06:50 +0800

    add tailwind project

---
+++ my-project/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className ="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div className="shrink-0">
+      
+    </div>
+    <div>
+      <div className="text-xl font-medium text-primary">ChitChat</div>
+      <p className="text-blue-450">You have a new message!</p>
+    </div>
+  </div>
+  );
+}
+
+export default App;
